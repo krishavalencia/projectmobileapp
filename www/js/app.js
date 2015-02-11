cd61c08ad727f052d5ee9078757feabb3faf214e
krishavalencia <kakaivalencia_21@yahoo.com>
2015-02-11 10:23:02 -0800

    Pinaka original na talaga

--- www/js/app.js
+++ www/js/app.js
@@ -21,30 +21,4 @@ angular.module('starter', ['ionic','ngCordova', 'starter.controllers'])
   });
 });
 
-<<<<<<< HEAD
-//   .controller("ourController", function($scope, $cordovaBarcodeScanner, $http){
-//   $scope.scanBarcode = function(){
-//     $cordovaBarcodeScanner.scan().then(function(imageData){
 
-
-
-//      $http.post(
-//     'https://api.mongolab.com/api/1/databases/testmobile/collections/savehere?apiKey=X8645ILWJXiV_Rmu4gZVn1URuu1WF1Ey', 
-//     $scope.data = JSON.stringify(imageData.text)
-  
-//   )
-//   .success(function(){
-//     console.log(arguments);
-//     console.log("yes naman!");
-//   })
-//   .error(function(){
-//     console.log(arguments);
-//     console.log("awwww");
-//   });
-
-//   });
-
-// }
-// });
-=======
->>>>>>> This is the original working version for scanner (with accordance with qr code format)
